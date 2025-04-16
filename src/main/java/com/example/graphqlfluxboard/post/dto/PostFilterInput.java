965d4108f75b2195b8681525b8f3d7591fd331a2
S-DPR <S-DPR@thanyouformuc@naver.com>
2025-04-16 17:00:37 +0900

    feat: Builder.default 추가

--- src/main/java/com/example/graphqlfluxboard/post/dto/PostFilterInput.java
+++ src/main/java/com/example/graphqlfluxboard/post/dto/PostFilterInput.java
@@ -12,10 +12,21 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class PostFilterInput {
+    @Builder.Default
     private FilterType type = FilterType.NONE;
+
+    @Builder.Default
     private String keyword = "";
+
+    @Builder.Default
     private Integer page = 1; // start with 1
+
+    @Builder.Default
     private Integer sizePerPage = 5;
+
+    @Builder.Default
     private String sortField = "createAt";
+
+    @Builder.Default
     private SortOrder sortOrder = SortOrder.DESC;
 }
