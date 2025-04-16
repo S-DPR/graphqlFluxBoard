7b448837830b057d34c3885beacd71a9eabed14d
S-DPR <S-DPR@thanyouformuc@naver.com>
2025-04-16 12:54:07 +0900

    refactoring: 미사용 필드 제거

--- src/main/java/com/example/graphqlfluxboard/common/exception/AuthException.java
+++ src/main/java/com/example/graphqlfluxboard/common/exception/AuthException.java
@@ -1,8 +1,7 @@
 package com.example.graphqlfluxboard.common.exception;
 
 public class AuthException extends RuntimeException {
-    public static String message = "Authentication Failed";
     public AuthException(String message) {
-        super("Authentication Failed: " + message);
+        super("Authentication: " + message);
     }
 }
