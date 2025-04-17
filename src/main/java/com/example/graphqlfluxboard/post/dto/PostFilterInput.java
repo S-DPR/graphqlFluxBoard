9a1e1e92a57f43d06808d6f2612012e4c2e284ed
S-DPR <S-DPR@thanyouformuc@naver.com>
2025-04-17 14:06:21 +0900

    feat: FilterDTO에서 Builder 제거 및 스키마에서 기본값 할당

--- src/main/java/com/example/graphqlfluxboard/post/dto/PostFilterInput.java
+++ src/main/java/com/example/graphqlfluxboard/post/dto/PostFilterInput.java
@@ -3,6 +3,7 @@ package com.example.graphqlfluxboard.post.dto;
 import com.example.graphqlfluxboard.common.validation.DTOValidationMessage;
 import com.example.graphqlfluxboard.post.enums.FilterType;
 import com.example.graphqlfluxboard.post.enums.SortOrder;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -13,29 +14,22 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class PostFilterInput {
-    @Builder.Default
     @NotNull(message = DTOValidationMessage.NOT_NULL_FILTER_TYPE)
     private FilterType type = FilterType.NONE;
 
-    @Builder.Default
     @NotNull(message = DTOValidationMessage.NOT_NULL_KEYWORD)
     private String keyword = "";
 
-    @Builder.Default
-    @Size(min = 1, message = DTOValidationMessage.PAGE_SIZE_LIMIT)
+    @Min(value = 1, message = DTOValidationMessage.PAGE_SIZE_LIMIT)
     private Integer page = 1; // start with 1
 
-    @Builder.Default
-    @Size(min = 1, message = DTOValidationMessage.POST_SIZE_PER_PAGE_LIMIT)
+    @Min(value = 1, message = DTOValidationMessage.POST_SIZE_PER_PAGE_LIMIT)
     private Integer sizePerPage = 5;
 
-    @Builder.Default
     @NotNull(message = DTOValidationMessage.POST_SORT_FIELD_ERROR)
     private String sortField = "createdAt";
 
-    @Builder.Default
     @NotNull(message = DTOValidationMessage.SORT_ORDER_ERROR)
     private SortOrder sortOrder = SortOrder.DESC;
 }
