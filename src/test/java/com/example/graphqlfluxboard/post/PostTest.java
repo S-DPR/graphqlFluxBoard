8abcd598831160253405f4063ed18e50b1135df6
S-DPR <thankyouformuch@naver.com>
2025-04-18 14:15:33 +0900

    feat: TestCode - Post 테스트코드 추가

--- src/test/java/com/example/graphqlfluxboard/post/PostTest.java
+++ src/test/java/com/example/graphqlfluxboard/post/PostTest.java
@@ -1,4 +1,68 @@
 package com.example.graphqlfluxboard.post;
 
-public class PostTest {
+import com.example.graphqlfluxboard.TestSupport;
+import com.example.graphqlfluxboard.common.exception.impl.AuthException;
+import com.example.graphqlfluxboard.common.exception.impl.NotFound;
+import com.example.graphqlfluxboard.post.domain.Post;
+import com.example.graphqlfluxboard.post.dto.DeletePostInput;
+import com.example.graphqlfluxboard.post.dto.SavePostInput;
+import com.example.graphqlfluxboard.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+public class PostTest extends TestSupport {
+    @BeforeEach
+    public void setup() {
+        super.setUp();
+    }
+
+    @Test
+    public void savePostWithWrongPW() {
+        User user = saveUser("TEST_USER", TEST_PASSWORD);
+        SavePostInput savePostInput = SavePostInput.builder()
+                .title("TEST_POST")
+                .userId(user.getId())
+                .password(WRONG_PASSWORD)
+                .content("CONTENT")
+                .build();
+        StepVerifier.create(postService.createPost(savePostInput))
+                .expectError(AuthException.class)
+                .verify();
+    }
+
+    @Test
+    public void deletePostWithWrongPW() {
+        User user = saveUser("TEST_USER", TEST_PASSWORD);
+        Post post = savePost("TEST_POST", "CONTENT", user.getId(), TEST_PASSWORD);
+
+        DeletePostInput deletePostInput = DeletePostInput.builder()
+                .postId(post.getId())
+                .password(WRONG_PASSWORD)
+                .build();
+        StepVerifier.create(postService.deletePost(deletePostInput))
+                .expectError(AuthException.class)
+                .verify();
+        StepVerifier.create(postService.findPostById(post.getId()))
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    public void deletePost() {
+        User user = saveUser("TEST_USER", TEST_PASSWORD);
+        Post post = savePost("TEST_POST", "CONTENT", user.getId(), TEST_PASSWORD);
+
+        DeletePostInput deletePostInput = DeletePostInput.builder()
+                .postId(post.getId())
+                .password(TEST_PASSWORD)
+                .build();
+        StepVerifier.create(postService.deletePost(deletePostInput))
+                .verifyComplete();
+        StepVerifier.create(postService.findPostById(post.getId()))
+                .expectError(NotFound.class)
+                .verify();
+    }
 }
