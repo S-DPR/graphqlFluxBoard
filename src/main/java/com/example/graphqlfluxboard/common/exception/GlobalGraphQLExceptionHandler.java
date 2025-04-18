a3951a65b3ec04c79cfed274b718675697e170a1
S-DPR <thankyouformuch@naver.com>
2025-04-18 11:23:16 +0900

    feat: Valid 에러핸들링 추가

--- src/main/java/com/example/graphqlfluxboard/common/exception/GlobalGraphQLExceptionHandler.java
+++ src/main/java/com/example/graphqlfluxboard/common/exception/GlobalGraphQLExceptionHandler.java
@@ -3,12 +3,17 @@ package com.example.graphqlfluxboard.common.exception;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -29,9 +34,25 @@ public class GlobalGraphQLExceptionHandler extends DataFetcherExceptionResolverA
                     .build();
         }
 
-        return GraphqlErrorBuilder.newError()
+        GraphQLError.Builder<?> builder = GraphQLError.newError()
                 .path(env.getExecutionStepInfo().getPath())
-                .location(env.getField().getSourceLocation())
+                .location(env.getField().getSourceLocation());
+
+        if (ex instanceof ConstraintViolationException e) {
+            String errors = e.getConstraintViolations().stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.joining(", "));
+
+            return builder
+                    .message("입력값이 잘못됐대요~: " + errors)
+                    .extensions(Map.of(
+                            "code", "BAD_REQUEST",
+                            "exception", ex.getClass().getSimpleName()
+                    ))
+                    .build();
+        }
+
+        return builder
                 .message("알 수 없는 에러래요~: " + ex.getMessage())
                 .extensions(Map.of(
                         "code", "INTERNAL_ERROR",
