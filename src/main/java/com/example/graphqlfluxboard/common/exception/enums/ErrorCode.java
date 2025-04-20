1498e9314b0a8f505b8578a26bbf1fea67b6b7b4
S-DPR <thankyouformuch@naver.com>
2025-04-20 09:48:24 +0900

    feat: Delete 실패시 에러 추가

--- src/main/java/com/example/graphqlfluxboard/common/exception/enums/ErrorCode.java
+++ src/main/java/com/example/graphqlfluxboard/common/exception/enums/ErrorCode.java
@@ -7,7 +7,8 @@ public enum ErrorCode {
     AuthenticationError("AUTHENTICATION_ERROR"),
     DuplicateError("DUPLICATE_ERROR"),
     NotFoundError("NOT_FOUND"),
-    NotSupportError("NOT_SUPPORT");
+    NotSupportError("NOT_SUPPORT"),
+    DeleteError("DELETE_ERROR"),;
 
     final String errorCode;
     ErrorCode(final String errorCode) {
---
+++ src/main/java/com/example/graphqlfluxboard/common/exception/impl/DeleteFailException.java
@@ -0,0 +1,22 @@
+package com.example.graphqlfluxboard.common.exception.impl;
+
+import com.example.graphqlfluxboard.common.exception.ApplicationError;
+import com.example.graphqlfluxboard.common.exception.enums.ErrorCode;
+import com.example.graphqlfluxboard.common.exception.enums.Resources;
+
+public class DeleteFailException extends ApplicationError {
+  private final Resources resources;
+  public DeleteFailException(Resources resources) {
+    this.resources = resources;
+  }
+
+  @Override
+  public String getErrorCode() {
+    return ErrorCode.DeleteError.getErrorCode();
+  }
+
+  @Override
+  public String getDefaultMessage() {
+    return resources.getValue() + ": 지우기 실패했대요~";
+  }
+}
