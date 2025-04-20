784995f5c57b08513ad1c2eb70b6415ca883e7ff
S-DPR <thankyouformuch@naver.com>
2025-04-20 10:17:44 +0900

    feat: Reply에 commentService 제거하고 ExistanceValidator로 comment 존재여부 검증 추가

--- src/main/java/com/example/graphqlfluxboard/reply/sevice/ReplyService.java
+++ src/main/java/com/example/graphqlfluxboard/reply/sevice/ReplyService.java
@@ -4,6 +4,7 @@ import com.example.graphqlfluxboard.comment.service.CommentService;
 import com.example.graphqlfluxboard.common.exception.impl.DeleteFailException;
 import com.example.graphqlfluxboard.common.exception.impl.NotFound;
 import com.example.graphqlfluxboard.common.exception.enums.Resources;
+import com.example.graphqlfluxboard.common.validation.ExistenceValidator;
 import com.example.graphqlfluxboard.reply.domain.Reply;
 import com.example.graphqlfluxboard.reply.dto.DeleteReplyInput;
 import com.example.graphqlfluxboard.reply.dto.SaveReplyInput;
@@ -23,7 +24,7 @@ import java.util.List;
 public class ReplyService {
     private final ReplyRepository replyRepository;
     private final UserService userService;
-    private final CommentService commentService;
+    private final ExistenceValidator existenceValidator;
 
     public Flux<Reply> findAllReplies() {
         return replyRepository.findAll();
@@ -44,13 +45,8 @@ public class ReplyService {
 
     public Mono<Reply> createReply(SaveReplyInput saveReplyInput) {
         return userService.verify(saveReplyInput.getUserId(), saveReplyInput.getPassword())
-                .then(commentService.existsById(saveReplyInput.getCommentId()))
-                .flatMap(exist -> {
-                    if (exist) {
-                        return createReply(Reply.of(saveReplyInput));
-                    }
-                    return Mono.error(new NotFound(Resources.COMMENT));
-                });
+                .then(existenceValidator.validateCommentExists(saveReplyInput.getCommentId()))
+                .then(createReply(Reply.of(saveReplyInput)));
     }
 
     public Mono<Void> deleteReply(String replyId) {
